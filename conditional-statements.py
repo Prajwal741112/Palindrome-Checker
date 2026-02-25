8e2b2143f07a8d14971dbb67b17290eaadc9dab4
Prajwal G Hondad <yourgithubemail@gmail.com>
2026-02-26 01:32:47 +0530

    first commit

---
+++ conditional-statements.py
@@ -0,0 +1,49 @@
+# a=5
+# b=7
+# c=3
+
+# print(a>b and a>c)
+
+# a=5
+# b=7
+# c=3
+
+# print(a>b or a>c)
+# n=int(input("Enter the value"))
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# age = int(input("enter the age "))
+# if age >=18:
+#     print("eligible for votting")
+# else:
+#     print(" not eligible for votting")
+
+# age =int(input("Enter the age "))
+# if age >=18 and age<=60:
+#     print("can work")
+# else :
+#     print("cannot work")
+
+# gender = input("Enter the gender: ")
+
+# if (gender == "m" or gender == "M"):
+#     print("male")
+# else:
+#     print("female")
+   
+# vowels=input("Enter the vowels")
+# if (vowels =="a","e","i","o","u"):
+#    print("is an vowels")
+# else:
+#    print("is not an vowels")
+
+# a=int(input("maths"))
+# b=int(input("physics"))
+# c=int(input("chemistry"))
+# if a>=45 and b>=45 and c>=45:
+#     print("pass")
+# else:
+#     print("fail")
